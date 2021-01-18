c9f2f009b51f62b44b164bdc72e3fcaed13ffd90
ThomannS <77104534+ThomannS@users.noreply.github.com>
2021-01-18 17:08:23 +0100

    Add files via upload

---
+++ Plot4.R
@@ -0,0 +1,21 @@
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionRelated & coalRelated)
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
+
+library(ggplot2)
+
+#Using the ggplot2 plotting system
+ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+
+print(ggp)
+
+# Copy Plot in png-file
+dev.copy(device = png, filename = 'plot2.png', width = 500, height = 400)
+dev.off ()
