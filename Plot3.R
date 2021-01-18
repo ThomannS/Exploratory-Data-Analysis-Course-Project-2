88e8554f56915c73d3b4feb713f51654e72d7821
ThomannS <77104534+ThomannS@users.noreply.github.com>
2021-01-18 16:45:43 +0100

    Add files via upload

---
+++ Plot3.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+
+#Using the ggplot2 plotting system
+ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+# Copy Plot in png-file
+dev.copy(device = png, filename = 'plot2.png', width = 500, height = 400)
+dev.off ()
